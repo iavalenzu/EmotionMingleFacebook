249b24183ab640d1b401d442299a5439028b48fb
Ismael Valenzuela <iavalenzu@gmail.com>
2015-08-05 18:47:33 -0300

    Update

---
+++ app/Model/Like.php
@@ -0,0 +1,54 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+class Like extends AppModel {
+    
+    public $belongsTo = array(
+        'Friend' => array(
+            'className' => 'Friend',
+        ),
+        'User' => array(
+            'className' => 'User',
+        )
+    );    
+    
+    
+    public function saveUniqueLike($friend_id = null, $user_id = null, $source_id = null, $type = null)
+    {
+        if(is_null($friend_id) || is_null($user_id) || is_null($source_id) || is_null($type)){
+            return;
+        }
+        
+        $existingLike = $this->find('first', array(
+            'conditions' => array(
+                'Like.friend_id' => $friend_id,
+                'Like.user_id' => $user_id,
+                'Like.source' => $source_id,
+                'Like.type' => $type
+            ),
+            'recursive' => -1
+        ));
+        
+        if(!isset($existingLike) || empty($existingLike))
+        {
+            $newLike = array(
+                'friend_id' => $friend_id,
+                'user_id' => $user_id,
+                'source' => $source_id,
+                'type' => $type
+            );
+
+            $this->Like->create();
+            $this->Like->save($newLike);
+
+        }
+        
+    }
+    
+}
